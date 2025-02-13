551c845b5055822dec5a56cce7bf73cac5e31e37
Natan Foleto <natanfoleto2015@hotmail.com>
2025-02-12 21:14:19 -0300

    Update 12/02/2025 pt3

--- src/http/routes/reactions/create-comment-reaction.ts
+++ src/http/routes/reactions/create-comment-reaction.ts
@@ -54,6 +54,13 @@ export async function createCommentReaction(
       response.status(201).json({
         result: 'success',
         message: 'Comment reagido',
+        data: {
+          reaction: {
+            ...commentReaction,
+            type,
+            isOwner: true,
+          },
+        },
       })
 
       return
